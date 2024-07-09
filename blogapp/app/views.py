218ec027b082e1fd9a8c0e037ddd6e6304c16a4b
Mousam <jangal.mangal.com>
2024-07-09 22:59:07 +0530

    Home page created,tags

--- blogapp/app/views.py
+++ blogapp/app/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from app.models import Post
+from app.models import Post,Comments
+from app.forms import CommentForm
 # Create your views here.
+
+def index(request):
+    posts=Post.objects.all()
+    context={'posts':posts}
+    return render(request,'app/index.html',context)
 
 def post_page(request,slug):
     post=Post.objects.get(slug=slug)
-    context={'post':post}
+    comments=Comments.objects.filter(post=post)
+    form=CommentForm()
+    if request.POST:
+        comment_form=CommentForm(request.POST)
+        if comment_form.is_valid:
+            comment=comment_form.save(commit=False)
+            postid=request.POST.get('post_id')
+            post=Post.objects.get(id=postid)
+            comment.post=post
+            comment.save()
+
+    if post.view_count is None:
+        post.view_count=1
+    else:
+        post.view_count=post.view_count + 1
+    post.save()
+
+    context={'post':post,'form':form,'comments':comments}
     return render(request,'app/post.html',context)
